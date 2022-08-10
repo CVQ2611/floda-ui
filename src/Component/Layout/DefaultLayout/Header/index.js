fa191de290da103b85d94413f4161100c80f8f11
CVQ2611 <vuquangchung3@gmail.com>
2022-08-10 23:50:12 +0700

    Floda-ui Css Header-top

--- src/Component/Layout/DefaultLayout/Header/index.js
+++ src/Component/Layout/DefaultLayout/Header/index.js
@@ -1,5 +1,28 @@
+import style from './Header.module.scss';
+import classNames from 'classnames/bind';
+const cx = classNames.bind(style);
 function Header() {
-    return <h1>Header</h1>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('header-top')}>
+                <div className={cx('header-top__content')}>
+                    <div className={cx('header-top__content-left')}>
+                        <p>Welcome to Floda online store</p>
+                    </div>
+                    <div className={cx('header-top__content-right')}>
+                        <div className={cx('header-language')}>
+                            Language
+                            {/* icon */}
+                        </div>
+                        <div className={cx('header-social-list')}>
+                            <span className={cx('header-social-item')}>{/* icon */}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={cx('header-main')}></div>
+        </header>
+    );
 }
 
 export default Header;
