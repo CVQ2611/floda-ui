17a3e869253c5da178f48e248fe948494968999a
CVQ2611 <vuquangchung3@gmail.com>
2022-08-15 14:32:49 +0700

    floda-ui + tippy + fonticon

--- src/Component/Layout/DefaultLayout/Header/index.js
+++ src/Component/Layout/DefaultLayout/Header/index.js
@@ -1,5 +1,10 @@
 import style from './Header.module.scss';
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { faFacebookF, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import Tippy from '@tippyjs/react/headless';
+
 const cx = classNames.bind(style);
 function Header() {
     return (
@@ -12,15 +17,33 @@ function Header() {
                     <div className={cx('header-top__content-right')}>
                         <div className={cx('header-language')}>
                             Language
-                            {/* icon */}
+                            <FontAwesomeIcon icon={faAngleDown} className={cx('header-language-icon')} />
                         </div>
                         <div className={cx('header-social-list')}>
-                            <span className={cx('header-social-item')}>{/* icon */}</span>
+                            <Tippy content="Facebook">
+                                <button className={cx('btn')}>
+                                    <FontAwesomeIcon icon={faFacebookF} className={cx('header-social-item')} />
+                                </button>
+                            </Tippy>
+                            <Tippy content="Instagram">
+                                <button className={cx('btn')}>
+                                    <FontAwesomeIcon icon={faInstagram} className={cx('header-social-item')} />
+                                </button>
+                            </Tippy>
+                            <Tippy content="Twitter">
+                                <button className={cx('btn')}>
+                                    <FontAwesomeIcon icon={faTwitter} className={cx('header-social-item')} />
+                                </button>
+                            </Tippy>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className={cx('header-main')}></div>
+            <div className={cx('header-main')}>
+                <button className={cx('btn')}>
+                    <img src="http://mauweb.monamedia.net/floda/wp-content/uploads/2019/09/logo.png" alt="logo" />
+                </button>
+            </div>
         </header>
     );
 }
